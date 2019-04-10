a08c2b402b6b00ea2a917445f6e931e1fd1f1704
Eric <gonzerc526@gmail.com>
2019-04-10 02:32:59 -0700

    Added unity project with environment and lights, player, and navmesh enemy

---
+++ Special_and_Lighting/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if(!player)
+        {
+            Debug.Log("No Player object found");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
+	}
+}
---
+++ Special_and_Lighting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float cameraSpeed;
+    public float jumpRate;
+    public float maxPitch;
+    public float minPitch;
+    
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.forward * v * moveSpeed * Time.deltaTime;
+        Vector3 sidestep = transform.right * h * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement + sidestep);
+        
+    }
+
+    private void Update()
+    {
+        yaw += cameraSpeed * Input.GetAxis("Mouse X");
+        pitch -= cameraSpeed * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Goal":
+                Debug.Log("Made it to the Exit Portal!");
+                break;
+            case "Pit":
+                Debug.Log("You fell...");
+                break;
+            default:break;
+        }
+    }
+}
